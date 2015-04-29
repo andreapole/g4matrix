9724ef2d8b7bea8ef2c84ea2e83f5bdce7340600
Marco Pizzichemi <marco.pizzichemi@gmail.com>
2015-04-29 13:48:51 +0200

    First commit

---
+++ build/CMakeFiles/g4matrix.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/g4matrix.cc" "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/build/CMakeFiles/g4matrix.dir/g4matrix.cc.o"
+  "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/src/ConfigFile.cc" "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/build/CMakeFiles/g4matrix.dir/src/ConfigFile.cc.o"
+  "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/src/CreateTree.cc" "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/build/CMakeFiles/g4matrix.dir/src/CreateTree.cc.o"
+  "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/src/g4matrixActionInitialization.cc" "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/build/CMakeFiles/g4matrix.dir/src/g4matrixActionInitialization.cc.o"
+  "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/src/g4matrixDetectorConstruction.cc" "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/build/CMakeFiles/g4matrix.dir/src/g4matrixDetectorConstruction.cc.o"
+  "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/src/g4matrixEventAction.cc" "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/build/CMakeFiles/g4matrix.dir/src/g4matrixEventAction.cc.o"
+  "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/src/g4matrixPhysicsList.cc" "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/build/CMakeFiles/g4matrix.dir/src/g4matrixPhysicsList.cc.o"
+  "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/src/g4matrixPrimaryGeneratorAction.cc" "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/build/CMakeFiles/g4matrix.dir/src/g4matrixPrimaryGeneratorAction.cc.o"
+  "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/src/g4matrixRunAction.cc" "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/build/CMakeFiles/g4matrix.dir/src/g4matrixRunAction.cc.o"
+  "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/src/g4matrixStackingAction.cc" "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/build/CMakeFiles/g4matrix.dir/src/g4matrixStackingAction.cc.o"
+  "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/src/g4matrixSteppingAction.cc" "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/build/CMakeFiles/g4matrix.dir/src/g4matrixSteppingAction.cc.o"
+  "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/src/g4matrixSteppingVerbose.cc" "/home/marco/Universita/Cern/NewClearPEM/Simulations/DepoMatrix/g4matrix/build/CMakeFiles/g4matrix.dir/src/g4matrixSteppingVerbose.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/marco/Programs/rootsource/include"
+  "/home/marco/geant/install/include/Geant4"
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtCore"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtOpenGL"
+  "../include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
